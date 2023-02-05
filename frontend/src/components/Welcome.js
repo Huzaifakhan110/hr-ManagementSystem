85a1ca82915044fdeb0c93f4e643c224db24b8ad
Huzaifa Khan <huzaifakaimkhani1@gmail.com>
2023-02-05 18:05:30 +0500

    done

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { useSelector , useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom";
 import { logout } from '../state/reducers/userReducer';
+
 // import Navbar from './Navbar';
 const settings = ['Logout'];
 function Welcome() {
@@ -36,12 +37,38 @@ function Welcome() {
       <div className="col-lg-6 mb-5 mb-lg-0">
         <h1 className="my-5 display-3 fw-bold ls-tight">
           The best offer <br />
-          <span className="text-primary">for your business Welcome   {
+          <span className="text-primary">Welcome to the Human Resource team    {
                   getDataFromStor.value !== null ? ` ${getDataFromStor.value.name}` : 'Welcome' 
               }</span>
         </h1>
 
       </div>
+      <div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Employees</h5>
+      <p class="card-text"></p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">HR Team</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Salary</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
     </div>
   </div>
 </div>
